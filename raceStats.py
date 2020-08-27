0e94263ca1dc3c2b38149a4e9546ff6853cb9bce
BuildTools <lukasboelling002@gmail.com>
2020-08-27 00:08:57 -0400

    Use list for abilities
    Using a list to store the abilities instead of a concatenated string
    makes selecting and removing individual abilities much easier.
    
    The abilityString() method returns a string of the concatenated
    abilities.

--- raceStats.py
+++ raceStats.py
@@ -13,7 +13,7 @@ class player:
         self.intellect = intellect
         self.wisdom = wisdom
         self.charisma = charisma
-        self.ability = ""
+        self.abilities = []
         self.kin = race
         raceList = race.lower().split(' ')
         try:
@@ -36,14 +36,14 @@ class player:
         self.dexterity += 2
         self.size = 'Medium'
         self.speed = 30
-        self.ability = "\n-Darkvision (60 ft, can't discern colors)\n-Keen Senses: Proficiency in Perception\n-Fey Ancestry: You have advantage on saving throws against being charmed, and magic can’t put you to sleep.\n-Trance: Elves don't sleep, they only meditate for 4 hours, gaining full benefit of an 8-hour sleep.\n-Elf Weapon Training: Proficiency with longsword, shortsword, shortbow and long bow."
+        self.abilities = ["Darkvision (60 ft, can't discern colors)", "Keen Senses: Proficiency in Perception", "Fey Ancestry: You have advantage on saving throws against being charmed, and magic can’t put you to sleep.", "Trance: Elves don't sleep, they only meditate for 4 hours, gaining full benefit of an 8-hour sleep.", "Elf Weapon Training: Proficiency with longsword, shortsword, shortbow and long bow."]
         if self.subrace == "high":
             self.intellect += 1
-            self.ability += "\n-Cantrip: You know one cantrip of your choice from the wizard spell list. Intelligence is your spellcasting ability for it.\n-Extra language: You can speak, read, and write one extra language of your choice."
+            self.abilities += ["Cantrip: You know one cantrip of your choice from the wizard spell list. Intelligence is your spellcasting ability for it.", "Extra language: You can speak, read, and write one extra language of your choice."]
         if self.subrace == "wood":
             self.wisdom += 1
             self.speed = 35
-            self.ability += "\n-Mask of the Wild: You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena."
+            self.abilities += ["Mask of the Wild: You can attempt to hide even when you are only lightly obscured by foliage, heavy rain, falling snow, mist, and other natural phenomena."]
         else:
             print('Try again')
 
@@ -56,43 +56,46 @@ class player:
         self.charisma += 1
         self.size = 'Medium'
         self.speed = 30
-        self.ability = "\n-Languages: You can speak, read, and write Common and one extra language of your choice. \nHumans typically learn the languages of other peoples they deal with, including obscure dialects. \nThey are fond of sprinkling their speech with words borrowed from other tongues: Orc curses, Elvish musical expressions, Dwarvish military phrases, and so on."
+        self.abilities = ["Languages: You can speak, read, and write Common and one extra language of your choice. \nHumans typically learn the languages of other peoples they deal with, including obscure dialects. \nThey are fond of sprinkling their speech with words borrowed from other tongues: Orc curses, Elvish musical expressions, Dwarvish military phrases, and so on."]
 
     def dragonborn(self):
         self.strength += 2
         self.speed = 30
         self.size = 'Medium'
-        self.ability = '\n-Language: You can speak, read, and write Common and Draconic. Draconic is thought to be one of the oldest languages and is often used in the study of magic. The language sounds harsh to most other creatures and includes numerous hard consonants and sibilants.\n-Breath Weapon: You can use your action to exhale destructive energy. Your draconic ancestry determines the size, shape, and damage type of the exhalation. When you use your breath weapon, each creature in the area of the exhalation must make a saving throw, the type of which is determined by your draconic ancestry. The DC for this saving throw equals 8 + your Constitution modifier + your proficiency bonus. A creature takes 2d6 damage on a failed save, and half as much damage on a successful one. The damage increases to 3d6 at 6th level, 4d6 at 11th level, and 5d6 at 16th level. After you use your breath weapon, you can’t use it again until you complete a short or long rest.'
+        self.abilities = ["Language: You can speak, read, and write Common and Draconic. Draconic is thought to be one of the oldest languages and is often used in the study of magic. The language sounds harsh to most other creatures and includes numerous hard consonants and sibilants.", "Breath Weapon: You can use your action to exhale destructive energy. Your draconic ancestry determines the size, shape, and damage type of the exhalation. When you use your breath weapon, each creature in the area of the exhalation must make a saving throw, the type of which is determined by your draconic ancestry. The DC for this saving throw equals 8 + your Constitution modifier + your proficiency bonus. A creature takes 2d6 damage on a failed save, and half as much damage on a successful one. The damage increases to 3d6 at 6th level, 4d6 at 11th level, and 5d6 at 16th level. After you use your breath weapon, you can’t use it again until you complete a short or long rest."]
         if self.subrace == "black" or self.subrace == "copper":
-            self.ability += ("\n-Damage resistance to Acid")
+            self.abilities += ["Damage resistance to Acid"]
         if self.subrace == 'blue' or self.subrace == 'bronze':
-            self.ability += ("\n-Damage resistance to Lightning")
+            self.abilities += ["Damage resistance to Lightning"]
         if self.subrace == 'brass' or self.subrace == 'gold' or self.subrace == 'red':
-            self.ability += ("\n-Damage resistance to Fire")
+            self.abilities += ["Damage resistance to Fire"]
         if self.subrace == 'green':
-            self.ability += ("\n-Damage resistance to Poison")
+            self.abilities += ["Damage resistance to Poison"]
         if self.subrace == 'silver' or self.subrace == 'white':
-            self.ability += ("\n-Damage resistance to Cold")
+            self.abilities += ["Damage resistance to Cold"]
 
     def dwarves(self):
         self.constitution += 2
         self.speed = 25
         self.size = 'Medium'
-        self.ability = "\n-Darkvision (60ft range, can't discern colors)" \
-                       "\n-Dwarven Resilience: You have advantage on saving throws against poison, and you have resistance against poison damage" \
-                       "\n-Dwarven Combat Training: You have proficiency with the battleaxe, handaxe, light hammer, and warhammer." \
-                       "\n-Tool Proficiency: You gain proficiency with the artisan’s tools of your choice: smith’s tools, brewer’s supplies, or mason’s tools." \
-                       "\n-Stonecunning: Whenever you make an Intelligence (**History**) check related to the origin of stonework, you are considered proficient in the History skill and add double your proficiency bonus to the check, instead of your normal proficiency bonus." \
-                       "\n-Languages: You can speak, read, and write Common and Dwarvish. Dwarvish is full of hard consonants and guttural sounds, and those characteristics spill over into whatever other language a dwarf might speak."
+        self.abilities = ["Darkvision (60ft range, can't discern colors)",
+                        "Dwarven Resilience: You have advantage on saving throws against poison, and you have resistance against poison damage",
+                        "Dwarven Combat Training: You have proficiency with the battleaxe, handaxe, light hammer, and warhammer.",
+                        "Tool Proficiency: You gain proficiency with the artisan’s tools of your choice: smith’s tools, brewer’s supplies, or mason’s tools.",
+                        "Stonecunning: Whenever you make an Intelligence (**History**) check related to the origin of stonework, you are considered proficient in the History skill and add double your proficiency bonus to the check, instead of your normal proficiency bonus.",
+                        "Languages: You can speak, read, and write Common and Dwarvish. Dwarvish is full of hard consonants and guttural sounds, and those characteristics spill over into whatever other language a dwarf might speak."]
         if self.subrace == "hill":
             self.wisdom += 1
-            self.ability += "\n-Dwarven Toughness: Your hit point maximum increases by 1, and it increases by 1 every time you gain a level."
+            self.abilities += ["Dwarven Toughness: Your hit point maximum increases by 1, and it increases by 1 every time you gain a level."]
         if self.subrace == 'mountain':
             self.strength += 2
-            self.ability += "\n-Dwarven Armor Training: You have proficiency with light and medium armor."
+            self.abilities += ["Dwarven Armor Training: You have proficiency with light and medium armor."]
 
     def showStat(self):
-        return (f'level {self.level} {self.name}, {self.kin}-kin\nMovement Speed: {self.speed} ft, Size: {self.size}\nAbilities Scores:\nStrength: {self.strength}\nDexterity: {self.dexterity}\nConstitution: {self.constitution}\nIntellect: {self.intellect}\nWisdom: {self.wisdom}\nCharisma: {self.charisma}\nAbilities: {self.ability}')
+        return (f'level {self.level} {self.name}, {self.kin}-kin\nMovement Speed: {self.speed} ft, Size: {self.size}\nAbilities Scores:\nStrength: {self.strength}\nDexterity: {self.dexterity}\nConstitution: {self.constitution}\nIntellect: {self.intellect}\nWisdom: {self.wisdom}\nCharisma: {self.charisma}\nAbilities: {self.abilities}')
+
+    def abilityString(self):
+        return '\n'.join(f'-{ability}' for ability in self.abilities)
 
     def levelUp(self):
         self.level += 1
